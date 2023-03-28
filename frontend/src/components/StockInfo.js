d9262d11444f37656585b7db31effec180e1252e
dyerfire9 <canadianraza@yahoo.com>
2023-03-28 12:50:57 -0400

    Added Realtime Stock buying and selling

--- frontend/src/components/StockInfo.js
+++ frontend/src/components/StockInfo.js
@@ -1,3 +1,6 @@
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import { useStocksContext } from "../hooks/useStocksContext"
+
 // const finnhub = require('finnhub');
 
 // // Finhub Stocks API 
@@ -7,14 +10,26 @@
 
 
 const StockInfo = ({stock}) => {
+    const {dispatch} = useStocksContext()
 
+    async function handleClick() {
+        const response = await fetch('/api/stocks/' + stock._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
 
+        if (response.ok){
+            dispatch({type: 'SELL_STOCK', payload: json})
+        }
+    }
 
     return (
         <div className="stock-info">
             <h4>{stock.ticker}</h4>
             <p><strong>No of Shares: </strong>{stock.shares}</p>
             <p><strong>Buy Price $: </strong>{stock.cost}</p>
+            <p>{formatDistanceToNow(new Date(stock.createdAt), {addSuffix: true})}</p>
+           <span onClick={handleClick}>Sell Stock</span>
         </div>
     )
 }
