2a5116fcb52b60a5de9fe7ee234a0bee87843fbc
dyerfire9 <canadianraza@yahoo.com>
2023-03-28 00:31:06 -0400

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // Import express
 const express = require('express');
+const mongoose = require('mongoose');
 
 require('dotenv').config()
 
@@ -28,8 +29,17 @@ app.get('/', (req, res) => {
     res.json({msg: 'Welcome to the app'})
 })
 
-
-
-app.listen(process.env.PORT, () => {
+//connect to mongo
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(process.env.PORT, () => {
     console.log(`Connected to db & listening on port ${process.env.PORT}`)
 })
+
+})
+.catch((error) => {
+    console.log(error)
+})
+
+
+
